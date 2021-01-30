cad11b4185555c31d3ba285f65a4bcf871696d42
LAPTOP-B65IV375\alex <Alex@schjermer.dk>
2021-01-30 23:46:21 +0100

    Character movement based on seb

---
+++ Imperceptible/Assets/Scripts/UI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UI : MonoBehaviour {
+    private Canvas _canvas;
+    public SpriteRenderer pencil;
+    void Start() {
+        _canvas = this.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)) {
+            var mpos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            
+        }
+    }
+}
